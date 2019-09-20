9eea2e016cf8a8eb72321a57a8180a41e7757327
yiruan@ebay.com <yiruan@ebay.com>
2019-09-20 14:33:45 +0800

    use the env parms for app name and pwd

--- part-5/实战1-电影搜索服务/movie-search/ingest_tmdb_to_appserarch.py
+++ part-5/实战1-电影搜索服务/movie-search/ingest_tmdb_to_appserarch.py
@@ -1,6 +1,9 @@
 import requests
 import json
+import os
 
+APP_NAME= os.getenv('APP_SEARCH_NAME')
+APP_PWD = os.getenv('APP_SEARCH_PWD')
 
 def extract():
     f = open('./tmdb.json')
@@ -19,8 +22,8 @@ def index_all(movieDict={}):
 def index_doc(doc):
     content=[]
     content.append(doc)
-    resp = requests.post("http://localhost:3002/api/as/v1/engines/tmdb/documents", 
-        headers={"content-type":"application/json","Authorization": "Bearer private-o5waw7vawazryrxce6ktu6dr"}, 
+    resp = requests.post("http://localhost:3002/api/as/v1/engines/"+APP_NAME+"/documents", 
+        headers={"content-type":"application/json","Authorization": "Bearer "+APP_PWD}, 
         data=json.dumps(content))
     print resp
 
